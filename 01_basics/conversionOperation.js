5b8b81a3996fdaec58347ab1316f4a935fca8892
Arjun Panday <157625713+arjunpanday19@users.noreply.github.com>
2026-01-19 04:01:42 +0000

    conversionOperation completed

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,11 +1,11 @@
 let score = 33;
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let age= "33";
-console.log(typeof age);
+// console.log(typeof age);
 let valueInNumber = Number(age); // it convert string into number
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 
 //"33" => 33
@@ -14,3 +14,19 @@ console.log(typeof valueInNumber);
 
 //"" => false
 //"ABC" => true
+
+//*********************************OPERATION************************************ */
+
+// console.log(2+2);    // addition 
+// console.log(2-2);  //substracation
+// console.log(2*2);    // multiplication
+// console.log(2/3);      //division
+// console.log(Math.floor(5/2));   //fllor division gives integer value
+// console.log(9%2);   //modulo operatore gives remainder
+// console.log(9**2); //power
+
+
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+
