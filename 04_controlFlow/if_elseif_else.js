bd39e86f30b8921049a0eb47c7db25ceebe83d0f
Arjun Panday <157625713+arjunpanday19@users.noreply.github.com>
2026-01-28 19:17:17 +0000

    if-elseif-else statements

---
+++ 04_controlFlow/if_elseif_else.js
@@ -0,0 +1,16 @@
+// if, else if, else statements in JavaScript
+// before control flow we have to know about some basic operator
+
+// >>Relational Operator
+// <  a<b (a is less than b)
+// >  a>b (a is grater than b)
+// <= a <=b (a is less than or equal to b)
+// >= a >=b (a is greater than or equal to b)
+// == a ==b (), != , === , !==
+
+// Logical Operator
+
+// && -> AND it return true if both contion is true
+// || -> OR it return true if any one condition is true
+
+
