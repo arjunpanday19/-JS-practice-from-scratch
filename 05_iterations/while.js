3f91f8ef256cc3710b54bca4c91c7ad99d42893c
Arjun Panday <157625713+arjunpanday19@users.noreply.github.com>
2026-01-28 20:16:42 +0000

    while loop

--- 05_iterations/while.js
+++ 05_iterations/while.js
@@ -23,3 +23,23 @@ while (i <= num) {
     i++;
 }
 // console.log(`Sum of first ${num} natural numbers is: ${total}`); // 55
+
+// Nested while loop example
+let a = 1;
+while (a <= 3) {
+    let b = 1;
+    while (b <= 2) {
+        // console.log(`a: ${a}, b: ${b}`);
+        b++;
+    }
+    a++;
+}
+// This will print:
+// a: 1, b: 1
+// a: 1, b: 2
+// a: 2, b: 1
+// a: 2, b: 2
+// a: 3, b: 1
+// a: 3, b: 2
+
+//+++++++++++++++++++++++++++++++
